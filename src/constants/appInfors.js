7463ca105395f906b752a3f8554d0bf4e0a906ee
Huỳnh Khánh Duy <103752303+GreenDyy@users.noreply.github.com>
2024-08-08 15:38:36 +0700

    them .env chứa base url

--- src/constants/appInfors.js
+++ src/constants/appInfors.js
@@ -5,5 +5,8 @@ export const appInfors = {
         WIDTH: Dimensions.get('window').width,
         HEIGHT: Dimensions.get('window').height,
     },
-    BASE_URL: 'http://192.168.1.20:3001'
+    // BASE_URL: 'http://192.168.1.20:3001'
+    BASE_URL: process.env.BASE_URL
+
+    //tuỳ máy sẽ phải thay đổi
 }   
